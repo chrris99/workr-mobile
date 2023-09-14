c00d7121247ca92136009174b184f8b78f73f044
chrris99 <conforti.christian@gmail.com>
2023-09-14 18:22:58 +0200

    add exercise muscles form

---
+++ src/components/exercise/forms/ExerciseMusclesForm.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import Text from "../../../design-system/typography/Text";
+
+export const ExerciseMuscleForm = () => {
+  return (
+    <View>
+      <Text>Exercise muscle form</Text>
+    </View>
+  );
+};
