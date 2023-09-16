4e30d8f5de2f9c0ebc6dd8b0ad106f2e01727692
chrris99 <conforti.christian@gmail.com>
2023-09-16 14:46:25 +0200

    add cov report

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "jest";
 const config: Config = {
   verbose: true,
   preset: "jest-expo",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
 };
 
 export default config;
