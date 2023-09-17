df0bb42355241594c67606ece28670ad91b0dedb
chrris99 <conforti.christian@gmail.com>
2023-09-17 11:23:58 +0200

    implement select input unit tests

---
+++ src/components/base/input/select/__tests__/MultiSelectItem.test.tsx
@@ -0,0 +1,99 @@
+import { fireEvent, render } from "@testing-library/react-native";
+import { Item } from "../types";
+import { MultiSelectItem } from "../MultiSelectItem";
+import { SELECT_ITEM_TEST_IDS } from "../SelectItem";
+
+const mockItem: Item<string> = {
+  value: "abs",
+  displayText: "Abs",
+  iconName: "Abs",
+};
+
+const mockOnChange = jest.fn();
+const mockSetSelectedItems = jest.fn();
+
+describe("multi select item", () => {
+  afterEach(() => jest.clearAllMocks());
+
+  describe("on press", () => {
+    describe("item is selected", () => {
+      it("should call on change prop with removed item", () => {
+        const selectedItems = ["abs", "chest"];
+
+        const { getByTestId } = render(
+          <MultiSelectItem
+            item={mockItem}
+            onChange={mockOnChange}
+            selectedItems={selectedItems}
+            setSelectedItems={mockSetSelectedItems}
+          />
+        );
+
+        const selectItem = getByTestId(SELECT_ITEM_TEST_IDS.SELECT_ITEM);
+        fireEvent.press(selectItem);
+
+        expect(mockOnChange).toBeCalledTimes(1);
+        expect(mockOnChange).toBeCalledWith(["chest"]);
+      });
+
+      it("should call set selected items prop with removed item", () => {
+        const selectedItems = ["abs", "chest"];
+
+        const { getByTestId } = render(
+          <MultiSelectItem
+            item={mockItem}
+            onChange={mockOnChange}
+            selectedItems={selectedItems}
+            setSelectedItems={mockSetSelectedItems}
+          />
+        );
+
+        const selectItem = getByTestId(SELECT_ITEM_TEST_IDS.SELECT_ITEM);
+        fireEvent.press(selectItem);
+
+        expect(mockSetSelectedItems).toBeCalledTimes(1);
+        expect(mockSetSelectedItems).toBeCalledWith(["chest"]);
+      });
+    });
+
+    describe("item is not selected", () => {
+      it("should call on change prop with added item", () => {
+        const selectedItems = ["chest"];
+
+        const { getByTestId } = render(
+          <MultiSelectItem
+            item={mockItem}
+            onChange={mockOnChange}
+            selectedItems={selectedItems}
+            setSelectedItems={mockSetSelectedItems}
+          />
+        );
+
+        const selectItem = getByTestId(SELECT_ITEM_TEST_IDS.SELECT_ITEM);
+        fireEvent.press(selectItem);
+
+        expect(mockOnChange).toBeCalledTimes(1);
+        expect(mockOnChange).toBeCalledWith(["abs", "chest"]);
+      });
+
+      it("should call set selected items prop with added item", () => {
+        const selectedItems = ["chest"];
+
+        const { getByTestId } = render(
+          <MultiSelectItem
+            item={mockItem}
+            onChange={mockOnChange}
+            selectedItems={selectedItems}
+            setSelectedItems={mockSetSelectedItems}
+          />
+        );
+
+        const selectItem = getByTestId(SELECT_ITEM_TEST_IDS.SELECT_ITEM);
+        fireEvent.press(selectItem);
+
+        expect(mockSetSelectedItems).toBeCalledTimes(1);
+        expect(mockSetSelectedItems).toBeCalledWith(["abs", "chestc"]);
+      });
+    });
+  });
+});
---
+++ src/components/base/input/select/__tests__/SelectItem.test.tsx
@@ -0,0 +1,44 @@
+import { fireEvent, render } from "@testing-library/react-native";
+import { SELECT_ITEM_TEST_IDS, SelectItem } from "../SelectItem";
+import { Item } from "../types";
+
+const ABS_ITEM: Item<string> = {
+  value: "abs",
+  iconName: "Abs",
+  displayText: "Abs",
+};
+
+const mockOnPress = jest.fn();
+
+describe("select item", () => {
+  describe("in selected state", () => {
+    it("should render correctly", () => {
+      const component = render(
+        <SelectItem item={ABS_ITEM} isSelected={true} onPress={mockOnPress} />
+      );
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe("in unselected state", () => {
+    it("should render correctly", () => {
+      const component = render(
+        <SelectItem item={ABS_ITEM} isSelected={false} onPress={mockOnPress} />
+      );
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe("on press", () => {
+    it("should call on press prop", () => {
+      const { getByTestId } = render(
+        <SelectItem item={ABS_ITEM} isSelected={false} onPress={mockOnPress} />
+      );
+
+      const selectItem = getByTestId(SELECT_ITEM_TEST_IDS.SELECT_ITEM);
+      fireEvent.press(selectItem);
+
+      expect(mockOnPress).toBeCalledTimes(1);
+    });
+  });
+});
